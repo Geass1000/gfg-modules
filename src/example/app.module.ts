17df31ec06ca1a55d922e1e593f4d43d3fbd0bda
Geass1000 <geass1000@gmail.com>
2019-07-14 23:17:12 +0300

    chore(example): add `Profile` module to import

--- src/example/app.module.ts
+++ src/example/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import * as Gfg from '../core';
+import { ProfileModule } from './profile/profile.module';
 
 @Gfg.Module({
-    imports: [ CoreModule ],
+    imports: [ CoreModule, ProfileModule ],
     providers: [ AppService, HomeComponent, AppComponent ],
 })
 export class AppModule {}
