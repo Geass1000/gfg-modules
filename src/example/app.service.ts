8c536ca2dab6af6848e9a954f96d73cc8f6f9a56
Andrey <geass1000@gmail.com>
2019-07-15 12:07:20 +0300

    feat(example): add `Inject` prop and param

--- src/example/app.service.ts
+++ src/example/app.service.ts
@@ -1,9 +1,17 @@
-import { LoggerService } from './core/logger.service';
 import * as Gfg from '../core';
+import { Inject } from '../core';
+
+import { LoggerService } from './core/logger.service';
+import { HttpService } from './core/http.service';
 
 @Gfg.Injectable()
 export class AppService {
+  @Inject(LoggerService)
+  private megaLogger: any;
 
-    constructor (private logger: LoggerService) {  }
+  constructor (
+    private logger: LoggerService,
+    @Inject(HttpService) private http: number,
+  ) { }
 }
 
