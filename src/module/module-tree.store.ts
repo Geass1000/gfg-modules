6a5cb066d51c2dc163e9ea45c6cae64e946238cc
Andrey <geass1000@gmail.com>
2019-08-22 01:20:20 +0300

    feat(module): add `Getter` and `Setter` for stored nodes

--- src/module/module-tree.store.ts
+++ src/module/module-tree.store.ts
@@ -1,14 +1,27 @@
 import { TreeNode } from '../core/tree';
 
 export class ModuleTreeStore {
-  private globalNodes: TreeNode[];
-  private appNode: TreeNode;
+  private _globalNodes: TreeNode[];
+  public get globalNodes (): TreeNode[] {
+    return this._globalNodes;
+  }
+  public set globalNodes (value: TreeNode[]) {
+    this._globalNodes = value;
+  }
+
+  private _appNode: TreeNode;
+  public get appNode (): TreeNode {
+    return this._appNode;
+  }
+  public set appNode (value: TreeNode) {
+    this._appNode = value;
+  }
 
   constructor (
     globalNodes: TreeNode[],
     appNode: TreeNode,
   ) {
-    this.globalNodes = globalNodes;
-    this.appNode = appNode;
+    this._globalNodes = globalNodes;
+    this._appNode = appNode;
   }
 }
